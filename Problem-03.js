014a529b0e13ef68815ac6eb082e3fdd13de2a2d
Reazul87 <hasanreazul87@gmail.com>
2025-08-14 13:13:19 +0600

    JAVASCRIPT_PROBLEM_3_ASSIGNMENT

---
+++ Problem-03.js
@@ -0,0 +1,18 @@
+// Start: Problem-03 : FIFA Best Team Award ?
+
+function bestTeam(player1, player2) {
+  // You have to write your code here
+  if (typeof player1 != "object" || typeof player2 != "object") {
+    return "Invalid";
+  }
+
+  let fareTeam1 = player1.foul + player1.cardR + player1.cardY;
+  let fareTeam2 = player2.foul + player2.cardR + player2.cardY;
+
+  if (fareTeam1 < fareTeam2) {
+    return player1.name;
+  } else if (fareTeam1 == fareTeam2) {
+    return "tie";
+  }
+  return player2.name;
+}
