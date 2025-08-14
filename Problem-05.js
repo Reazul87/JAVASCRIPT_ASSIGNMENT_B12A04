ece496e637577d1ba0993c31b7ca0c107a6bd0a6
Reazul87 <hasanreazul87@gmail.com>
2025-08-14 13:13:47 +0600

    JAVASCRIPT_PROBLEM_5_ASSIGNMENT

---
+++ Problem-05.js
@@ -0,0 +1,28 @@
+// Start: Problem-05: Exam Result Report Generator ?
+
+function resultReport(marks) {
+  // You have to write your code here
+  if (Array.isArray(marks) === false) {
+    return "Invalid";
+  }
+
+  let totalMarks = 0;
+  let passed = 0;
+  let failed = 0;
+
+  for (let mark of marks) {
+    totalMarks += mark;
+
+    if (mark >= 40) {
+      passed++;
+    } else {
+      failed++;
+    }
+  }
+
+  let average = totalMarks / marks.length;
+  let finalScore = Math.round(average);
+
+  let reportCard = { finalScore: finalScore, pass: passed, fail: failed };
+  return reportCard;
+}
